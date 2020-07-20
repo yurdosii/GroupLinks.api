95410c1d4966f6bd5eb164d05d02b9867d7e04be
Yurii <yurdosii.ksv@gmail.com>
2020-07-20 18:26:04 +0300

    Refactoring

--- src/groups/views.py
+++ src/groups/views.py
@@ -9,7 +9,7 @@ from .models import Group, Link
 from .serializers import GroupWithNestedSerializer, LinkWithNestedSerializer
 
 
-class GroupViewSet(viewsets.ModelViewSet):
+class GroupViewSet(viewsets.ModelViewSet):  # pylint: disable=too-many-ancestors
     """
     Group viewset
     """
@@ -25,16 +25,18 @@ class GroupViewSet(viewsets.ModelViewSet):
     # def update(self, request, pk=None):
     #     pass
 
-    # TODO - delete later
     @action(detail=False) # "/groups/recent_groups"
     def recent_groups(self, request):
+        """
+        TODO Delete this method later
+        """
         recent_groups = Group.objects.all().order_by('-name')
 
         serializer = self.get_serializer(recent_groups, many=True)
         return Response(serializer.data)
 
 
-class LinkViewSet(viewsets.ModelViewSet):
+class LinkViewSet(viewsets.ModelViewSet):  # pylint: disable=too-many-ancestors
     """
     Link viewset
     """
