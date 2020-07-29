8330e9e352a2f8cf9c24d32f1568005c377d8047
Yurii <yurdosii.ksv@gmail.com>
2020-07-29 19:32:46 +0300

    Refactoring

--- src/groups/views.py
+++ src/groups/views.py
@@ -6,7 +6,6 @@ from rest_framework import viewsets, permissions
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import status
-# from django.shortcuts import get_object_or_404
 from .models import Group, Link
 from .serializers import GroupWithNestedSerializer, LinkWithNestedSerializer
 
